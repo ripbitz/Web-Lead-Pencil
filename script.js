ec38e37a2e01022a356bbec464dc08ebcaee4676
MarioTaglic <M.Taglic11@gmail.com>
2020-12-24 00:48:39 -0500

    Added features

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
- // Canvas
- let c = document.getElementById("canvas");
- let ctx = c.getContext('2d');
- c.width = window.innerWidth * 1.25;
- c.height = window.innerHeight * 1.25;
+// Canvas
+let c = document.getElementById("canvas");
+let ctx = c.getContext('2d');
 
- c.style.width = window.innerWidth + "px";
- c.style.height = window.innerHeight + "px";
-
- // Components
- function lead(x, y, size, velocity){
-   this.x = x;
-   this.y = y;
-   this.size = size;
-   this.velocity = velocity;
- }
+// Lead component
+function lead(x, y, size, velocity){
+  this.x = x;
+  this.y = y;
+  this.size = size;
+  this.velocity = velocity;
+}
 
  // Init array of lead pieces
  let leadPieces = [];
@@ -21,17 +16,22 @@
  // Init click counter
  let clickCounter = document.getElementById('clickCounter')
  let cc = 0;
+
+ // Init total
+ let totalAmountOfLead = 0;
+
+ // Flag for auto mode
+ let AUTO_MODE = false;
+
+ // FLag for endless mode
+ let ENDLESS_MODE = false;
  
  // Mechanics
- const mechanics = {
+const mechanics = {
   // Draw pen
   drawLeadPencil: () => {
     // Body
     ctx.beginPath();
-    ctx.shadowBlur = 6;
-    ctx.shadowOffsetX = 3;
-    ctx.shadowOffsetY = 3;
-    ctx.shadowColor = '#404040';
     ctx.rect(0, c.height/2, 200, 20);
     ctx.fillStyle = '#8B0000';
     ctx.fill();
@@ -50,14 +50,18 @@
     // Clip
     ctx.fillStyle = '#C0C0C0';
     ctx.beginPath();
-    ctx.rect(0, c.height/2-15, 30, 6);
+    ctx.rect(0, c.height/2-12, 30, 6);
     ctx.fill();
     ctx.closePath();
-
+    ctx.fillStyle = '#C0C0C0';
+    ctx.beginPath();
+    ctx.rect(0, c.height/2-12, 5, 12);
+    ctx.fill();
+    ctx.closePath();
   },
 
   // Lead attributes
-  START_POINT: 273,
+  START_POINT: 270,
   MIN_SIZE: 15,
   MAX_SIZE: 30,
 
@@ -71,19 +75,19 @@
       ctx.closePath();
     }
   },
-  
-  // Add lead
+
+  // Add lead piece
   addLead: () => {
-    // Generate random size for lead
     let RANDOM_SIZE = Math.random() * (mechanics.MAX_SIZE - mechanics.MIN_SIZE) + mechanics.MIN_SIZE;
     let l = new lead(mechanics.START_POINT, c.height/2+10, RANDOM_SIZE, 0);
     mechanics.START_POINT += RANDOM_SIZE;
     leadPieces.push(l);
+    totalAmountOfLead += RANDOM_SIZE;
   },
 
   // Remove lead
-  removeLead: (a) => {
-    for(let i = 0; i<a; i++){
+  removeLead: (numOfPieces) => {
+    for(let i = 0; i<numOfPieces; i++){
       mechanics.START_POINT -= leadPieces[leadPieces.length-1].size;     
       leadPieces.pop();
     }
@@ -103,8 +107,8 @@
   // Flag for reaching the end of the window
   END_REACHED: false,
 
-  // Condition for game to be over
-  GAME_OVER: () => {
+  // Checking if there is no more lead
+  NO_MORE_LEAD: () => {
     return mechanics.MIN_SIZE === 0 && mechanics.MAX_SIZE === 0
   },
 
@@ -112,7 +116,7 @@
   CLICK_SOUND_EFFECT: new Audio('click.mp3'),
   BREAK_SOUND_EFFECT: new Audio('break.mp3'),
 
-  // method to push lead
+  // Method to push lead
   pushLead: () => {
     if(mechanics.START_POINT >= c.width){ 
       mechanics.END_REACHED = true;
@@ -121,21 +125,23 @@
         let randomBreakPoint = Math.random() * (leadPieces.length - 1) + 1;
         mechanics.removeLead(randomBreakPoint);
         mechanics.END_REACHED = false;
-        if(mechanics.MAX_SIZE  > 1){ mechanics.MAX_SIZE -= 5; }
-        if(mechanics.MIN_SIZE > 1 ){ mechanics.MIN_SIZE -= 5; }
-        if(mechanics.GAME_OVER()){
-          let gameOverMessage = document.getElementById('gameOverMessage');
-          gameOverMessage.textContent = "YOU ARE OUT OF LEAD :(";
-          mechanics.reset();
+        if(!ENDLESS_MODE){
+          if(mechanics.MAX_SIZE  > 1){ mechanics.MAX_SIZE -= 1; }
+          if(mechanics.MIN_SIZE > 1 ){ mechanics.MIN_SIZE -= 1; }
+        }
+        if(mechanics.NO_MORE_LEAD()){
+          let endMessage = document.getElementById('endMessage');
+          clickCounter.textContent = '';
+          endMessage.textContent = 'you are out of lead :( total size of all lead clicked: ' + totalAmountOfLead + " pixels";
         }
       }
     } 
     else {
-      if(!mechanics.END_REACHED && !mechanics.GAME_OVER()){
+      if(!mechanics.END_REACHED && !mechanics.NO_MORE_LEAD()){
         mechanics.CLICK_SOUND_EFFECT.play();
         mechanics.addLead();
         clickCounter.textContent = '';
-        clickCounter.textContent = "Lead Clicked: " + cc;
+        clickCounter.textContent = 'lead clicked: ' + cc + ' pieces';
         cc++;
       }  
     }
@@ -146,35 +152,53 @@
     mechanics.START_POINT = 273;
     mechanics.MIN_SIZE = 10;
     mechanics.MAX_SIZE = 30;
-    clickCounter.textContent = "";
+    clickCounter.textContent = 'lead clicked: ';
     cc = 0;
-    gameOverMessage.textContent = "";
+    endMessage.textContent = '';
+    AUTO_MODE = false;
+    ENDLESS_MODE = false;
+  },
+
+  resize: () => {
+    c.width = window.innerWidth * 1.25;
+    c.height = window.innerHeight * 1.25;
+    c.style.width = window.innerWidth + 'px';
+    c.style.height = window.innerHeight + 'px';
   }
 }
-
- c.req = requestAnimationFrame(loop);
 
  // Keyboard event listeners to push lead and reset the game
-document.addEventListener("keydown", keyDownHandler);
+document.addEventListener('keydown', keyDownHandler);
+
 function keyDownHandler(event) {
   switch (event.code) {
-    case "Space":
+    case 'Space':
       mechanics.pushLead();
       break;
-    case "KeyR":
+    case 'KeyR':
       mechanics.reset();
+      break;
+    case 'KeyA':
+      AUTO_MODE = true;
+      break;
+    case 'KeyE':
+      ENDLESS_MODE = true;
       break;
   }
 }
 
- function loop(){
-   ctx.clearRect(0,0, c.width, c.height);
-   mechanics.drawLeadPencil();
-   mechanics.drawLead();
-   // Auto mode (uncomment below)
-   //mechanics.pushLead();
-   body.onclick = () => {
-     mechanics.pushLead();
+c.req = requestAnimationFrame(loop);
+
+function loop(){
+  ctx.clearRect(0,0, c.width, c.height);
+  mechanics.resize();
+  mechanics.drawLeadPencil();
+  mechanics.drawLead();
+  body.onclick = () => {
+    mechanics.pushLead();
   }
-   c.req = requestAnimationFrame(loop);
- }
+  if(AUTO_MODE){
+    mechanics.pushLead();
+  }
+  c.req = requestAnimationFrame(loop);
+}
